2168097f398ff122f49ce7fc22d0f233b054d7bd
StackWitch <sadieevecoleman@gmail.com>
2025-06-28 15:56:02 -0500

    Create script.js

---
+++ Chapter3Project/script.js
@@ -0,0 +1,27 @@
+/*
+    Author: /.Sadie/
+    Date: 2025-06-28
+    WEB124 Chapter 3 Project JS
+*/
+
+const fullName = "Sadie Coleman";
+console.log("Full Name:", fullName);
+
+const desiredSalary = 75000; // example number
+console.log("Desired Salary:", desiredSalary);
+
+const isVeteran = false; // 
+console.log("Veteran Status:", isVeteran);
+
+const friendsNames = ["Alex", "Jordan", "Taylor"];
+console.log("Friends' Names:", friendsNames);
+
+const friendsSalaries = [68000, 72000, 70000];
+console.log("Friends' Salaries:", friendsSalaries);
+
+const anotherFriend = {
+    firstName: "Morgan",
+    lastName: "Lee",
+    desiredSalary: 69000
+};
+console.log("Another Friend:", anotherFriend);
